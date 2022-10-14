677e5b9c928cedd583ebcf8ccfaa95598969f00f
nrp-silva <nickolascaragua@gmail.com>
2022-10-13 23:11:34 -0300

    fix/create interface

--- src/components/Heading.tsx
+++ src/components/Heading.tsx
@@ -6,17 +6,27 @@ export interface HeadingProps {
   children: ReactNode
   size?: 'sm' | 'md' | 'lg'
   asChild?: boolean
+  className?: string
 }
 
-export function Heading({ children, asChild, size = 'md' }: HeadingProps) {
+export function Heading({
+  children,
+  asChild,
+  size = 'md',
+  className,
+}: HeadingProps) {
   const Comp = asChild ? Slot : 'h2'
   return (
     <Comp
-      className={clsx('text-gray-100 font-bold font-sans,', {
-        'text-lg': size === 'sm',
-        'text-xl': size === 'md',
-        'text-2xl': size === 'lg',
-      })}
+      className={clsx(
+        'text-gray-100 font-bold font-sans,',
+        {
+          'text-lg': size === 'sm',
+          'text-xl': size === 'md',
+          'text-2xl': size === 'lg',
+        },
+        className,
+      )}
     >
       {children}
     </Comp>
